2fc0df567492aee9be4a083abc0bb1ad69c10ece
Huntley <huntleycastner@gmail.com>
2023-02-26 11:45:27 -0800

    added business counters

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 let count = 0;
 let incrementAmount = 1;
+let lemonadeStandCount = 0;
+let lawnMowingCount = 0;
+let coffeeShopCount = 0;
+let fastFoodCount = 0;
+let bankCount = 0;
 
 function updateCounterText() {
   document.getElementById("dollarCounter").innerHTML = count.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  document.getElementById("lemonadeCount").textContent = lemonadeStandCount;
+  document.getElementById("mowerCount").textContent = lawnMowingCount;
+  document.getElementById("coffeeCount").textContent = coffeeShopCount;
+  document.getElementById("fastFoodCount").textContent = fastFoodCount;
+  document.getElementById("bankCount").textContent = bankCount;
 }
 
 function incrementCounter() {
@@ -14,6 +24,8 @@ function lemonadeStand() {
   if (count >= 10) {
     count -= 10;
     incrementAmount += 0.25;
+    lemonadeStandCount++;
+    document.getElementById("lemonadeCount").textContent = lemonadeStandCount;
   }
   updateCounterText();
 }
@@ -22,6 +34,8 @@ function lemonadeStand() {
     if (count >= 100) {
       count -= 100;
       incrementAmount += 5.00;
+      lawnMowingCount++;
+      document.getElementById("mowerCount").textContent = lawnMowingCount;
     }
     updateCounterText();
   }
@@ -30,6 +44,8 @@ function lemonadeStand() {
     if (count >= 10000) {
       count -= 10000;
       incrementAmount += 100.00;
+      coffeeShopCount++;
+      document.getElementById("coffeeCount").textContent = coffeeShopCount;
     }
     updateCounterText();
   }
@@ -38,6 +54,8 @@ function lemonadeStand() {
     if (count >= 100000) {
       count -= 100000;
       incrementAmount += 1000.00;
+      fastFoodCount++;
+      document.getElementById("fastFoodCount").textContent = fastFoodCount;
     }
     updateCounterText();
   }
@@ -46,10 +64,18 @@ function lemonadeStand() {
     if (count >= 10000000) {
       count -= 10000000;
       incrementAmount += 100000.00;
+      bankCount++;
+      document.getElementById("bankCount").textContent = bankCount;
     }
     updateCounterText();
   }
 
+  
+  
+
+
+
   /*To do: #1 Add a counter for how many of each upgrade user has.
            #2 Change cost of upgrades to original cost + or * number of upgrades, whichever feels better.
-           #3 Test with friends.
+           #3 Test with friends 
+  */
