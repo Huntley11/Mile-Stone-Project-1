798847ac0741665cb418ba728892d2b0996ca543
Huntley <huntleycastner@gmail.com>
2023-02-26 14:49:08 -0800

    added updateCounters for each business, as well as incrementing the price of each business. I also switched the positing of the buttons.

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 let count = 0;
 let incrementAmount = 1;
-let lemonadeStandCount = 0;
-let lawnMowingCount = 0;
-let coffeeShopCount = 0;
+let lemonadeCount = 0;
+let lemonadeCost = 10;
+let mowerCount = 0;
+let mowerCost = 100;
+let coffeeCount = 0;
+let coffeeCost = 10000;
 let fastFoodCount = 0;
+let fastFoodCost = 100000;
 let bankCount = 0;
+let bankCost = 10000000;
 
 function updateCounterText() {
   document.getElementById("dollarCounter").innerHTML = count.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
-  document.getElementById("lemonadeCount").textContent = lemonadeStandCount;
-  document.getElementById("mowerCount").textContent = lawnMowingCount;
-  document.getElementById("coffeeCount").textContent = coffeeShopCount;
-  document.getElementById("fastFoodCount").textContent = fastFoodCount;
-  document.getElementById("bankCount").textContent = bankCount;
 }
 
 function incrementCounter() {
@@ -21,57 +21,65 @@ function incrementCounter() {
 }
 
 function lemonadeStand() {
-  if (count >= 10) {
-    count -= 10;
+  if (count >= lemonadeCost) {
+    count -= lemonadeCost;
     incrementAmount += 0.25;
-    lemonadeStandCount++;
-    document.getElementById("lemonadeCount").textContent = lemonadeStandCount;
+    lemonadeCount++;
+    lemonadeCost += lemonadeCount * 10;
   }
   updateCounterText();
+  document.getElementById("lemonadeCount").innerHTML = lemonadeCount;
+  document.getElementById("lemonadeButton").innerHTML = "Purchase A Lemonade Stand For " + lemonadeCost.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 }
 
-  function lawnMowing() {
-    if (count >= 100) {
-      count -= 100;
-      incrementAmount += 5.00;
-      lawnMowingCount++;
-      document.getElementById("mowerCount").textContent = lawnMowingCount;
-    }
-    updateCounterText();
+function lawnMowing() {
+  if (count >= mowerCost) {
+    count -= mowerCost;
+    incrementAmount += 5.00;
+    mowerCount++;
+    mowerCost += mowerCount * 100;
   }
+  updateCounterText();
+  document.getElementById("mowerCount").innerHTML = mowerCount;
+  document.getElementById("lawnMowingButton").innerHTML = "Hire A Person to Mow Lawns For " + mowerCost.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+}
 
-  function coffeeShop() {
-    if (count >= 10000) {
-      count -= 10000;
-      incrementAmount += 100.00;
-      coffeeShopCount++;
-      document.getElementById("coffeeCount").textContent = coffeeShopCount;
-    }
-    updateCounterText();
+function coffeeShop() {
+  if (count >= coffeeCost) {
+    count -= coffeeCost;
+    incrementAmount += 100.00;
+    coffeeCount++;
+    coffeeCost += coffeeCount * 1000;
   }
+  updateCounterText();
+  document.getElementById("coffeeCount").innerHTML = coffeeCount;
+  document.getElementById("coffeeShopButton").innerHTML = "Purchase A Coffee Shop For " + coffeeCost.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+}
 
-  function fastFood() {
-    if (count >= 100000) {
-      count -= 100000;
-      incrementAmount += 1000.00;
-      fastFoodCount++;
-      document.getElementById("fastFoodCount").textContent = fastFoodCount;
-    }
-    updateCounterText();
+function fastFood() {
+  if (count >= fastFoodCost) {
+    count -= fastFoodCost;
+    incrementAmount += 1000.00;
+    fastFoodCount++;
+    fastFoodCost += fastFoodCount * 10000;
   }
+  updateCounterText();
+  document.getElementById("fastFoodCount").innerHTML = fastFoodCount;
+  document.getElementById("fastFoodButton").innerHTML = "Purchase A Fast Food Restaurant For " + fastFoodCost.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+}
 
-  function bank() {
-    if (count >= 10000000) {
-      count -= 10000000;
-      incrementAmount += 100000.00;
-      bankCount++;
-      document.getElementById("bankCount").textContent = bankCount;
-    }
-    updateCounterText();
+function bank() {
+  if (count >= bankCost) {
+    count -= bankCost;
+    incrementAmount += 100000.00;
+    bankCount++;
+    bankCost += bankCount * 1000000;
   }
+  updateCounterText();
+  document.getElementById("bankCount").innerHTML = bankCount;
+  document.getElementById("bankButton").innerHTML = "Purchase A Bank For " + bankCost.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+}
 
-  
-  
 
 
 
